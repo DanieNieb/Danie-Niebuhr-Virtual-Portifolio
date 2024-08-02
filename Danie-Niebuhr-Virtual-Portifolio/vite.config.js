fb818aa962dab840d35f5dea0153b533c15455d5
DanieNieb <danieniebuhr@gmail.com>
2024-08-02 14:30:24 +0200

    Deploy v0.5

--- Danie-Niebuhr-Virtual-Portifolio/vite.config.js
+++ Danie-Niebuhr-Virtual-Portifolio/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 // vite.config.js
 export default defineConfig({
-  base: 'Danie-Niebuhr-Virtual-Portifolio',
+  base: '/Danie-Niebuhr-Virtual-Portifolio/',
   plugins: [react()],
 });
