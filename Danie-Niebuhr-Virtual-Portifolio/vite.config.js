16fb3c1cd6f8cbb68959609f0d00839f456cd3a0
DanieNieb <danieniebuhr@gmail.com>
2024-08-02 14:49:17 +0200

    Final Commit

--- Danie-Niebuhr-Virtual-Portifolio/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-// vite.config.js
-export default defineConfig({
-  base: '/Danie-Niebuhr-Virtual-Portifolio/',
-  plugins: [react()],
-});
